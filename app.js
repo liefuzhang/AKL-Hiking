a2006d371307f7d8c6165b06b4617f1c906ef775
haizlin <80285586@qq.com>
2018-02-07 17:02:39 +0800

    添加原型图

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 App({
   onLaunch: function (options) {
     if (options.scene == 1044) {
-      console.log('场景值＝',options.shareTicket)
+      console.log('场景值＝', options.shareTicket)
     }
 
     // 展示本地存储能力
@@ -13,29 +13,27 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    wx.getUserInfo({
+      success: res => {
+        console.log('用户信息', res);
+        // 可以将 res 发送给后台解码出 unionId
+        this.globalData.userInfo = res.userInfo
+
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        if (this.userInfoReadyCallback) {
+          this.userInfoReadyCallback(res)
         }
       }
     })
+
   },
   ohShow: function (options) {
     console.log('show...', options)
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 let API = require('../../utils/api.js');
+const app = getApp();
 
 Page({
 
@@ -6,56 +7,85 @@ Page({
    * 页面的初始数据
    */
   data: {
-    actityNum: 0
+    actityNum: 0,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
@@ -63,31 +93,21 @@ Page({
    */
   onShareAppMessage: function (res) {
     // if (res.from === 'button') {
-      // 来自页面内转发按钮
-      console.log('分享结果：',res)
+    // 来自页面内转发按钮
+    console.log('分享结果：', res)
     // }
     return {
       // 我报名今晚的羽毛球活动，就差你了！
       title: '浪子神剑 邀请你来打羽毛球(2月2日,星期一,晚上20：00-22：00),目前已报3人，活动火热报名中……',
       path: '/pages/ranking/index?id=123',
-      imageUrl:'/assets/baoming.jpg',
+      imageUrl: '/assets/baoming.jpg',
       success: function (res) {
         // 转发成功
-        console.log('成功',res); 
+        console.log('成功', res);
         wx.showShareMenu({
           // 要求小程序返回分享目标信息
           withShareTicket: true
         });
-
-        wx.getShareInfo({
-          shareTicket: 'aaa',
-          success: function(res){
-            console.log(res.errMsg);
-            console.log(res.encryptedData);
-            console.log(res.iv);
-          }
-        });
-        console.log(222);
       },
       fail: function (res) {
         // 转发失败
